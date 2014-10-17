4adbd3f8779497676e2fee8fae96f10c6c3e3278
Thulio Philipe <thulioph@gmail.com>
2014-10-17 16:51:07 -0300

    Adds touch gestures in coconut :squirrel:

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,23 @@
 //= require vendor/bootstrap
+
+var pad = document.querySelector("#tap"),
+    coco = document.querySelector(".coco"),
+    coconut = document.querySelector("#coconut");
+
+var hammerPad = new Hammer(pad);
+
+hammerPad.on("tap press swipeup swipedown", function(event){
+  var pointer0 = event.pointers[0];
+
+  if (event.type.match(/tap|swipeup|swipedown|press/g)) {
+    var tap = event.tapCount;
+
+    coconut.classList.toggle('move');
+
+    if (tap == 3) {
+      console.log('YO!');
+      coco.classList.add('drop');
+    }
+  }
+
+});
