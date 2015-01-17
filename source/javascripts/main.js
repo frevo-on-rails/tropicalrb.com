80a94539a9988bd2fe8135d6a84c8ceca679ffce
Joel Cipriano <joelcipriano@fastmail.fm>
2015-01-17 14:54:51 -0300

    Improved main menu.

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -3,3 +3,15 @@
 //= require vendors/bootstrap
 
 //= require_tree .
+
+$(document).ready( function() {
+
+  $('.navbar-toggle').bind('click', function() {
+    if( !$('.navbar-collapse').hasClass('in') ) {
+      $('.navbar-brand').fadeIn(200);
+    } else {
+      $('.navbar-brand').fadeOut(300);
+    }
+  });
+
+});
