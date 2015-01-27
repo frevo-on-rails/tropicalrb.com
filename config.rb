d510d4bb17552daab19517c95f66d0d9954745a8
Sérgio Fontes <sergiofontes@gmail.com>
2015-01-27 10:14:47 -0300

    Activates directory indexes and updates navbar routes.

--- config.rb
+++ config.rb
@@ -55,6 +55,8 @@ set :images_dir, 'images'
 
 activate :autoprefixer
 
+activate :directory_indexes
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
